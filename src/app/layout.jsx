3e64a360f6fd146c538dc979b5b1ddaf75ff2214
rtrivaldo <rtrivaldo05@gmail.com>
2024-09-04 17:44:22 +0700

    add openGraph

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,6 +6,19 @@ const darkerGrotesque = Darker_Grotesque({ subsets: ["latin"] });
 export const metadata = {
     title: "ShortPath",
     description: "Tool to shorten a long link.",
+    openGraph: {
+        title: "ShortPath",
+        description: "Tool to shorten a long link.",
+        url: "https://www.shortpath.site/",
+        siteName: "ShortPath",
+        images: [
+            {
+                url: "/openGraphImage.jpeg",
+                width: 1260,
+                height: 800,
+            },
+        ],
+    },
 };
 
 export default function RootLayout({ children }) {
